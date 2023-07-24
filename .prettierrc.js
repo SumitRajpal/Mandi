825705d977e1b8c834e875985ef78e5ca1cc0437
SumitRajpal <sumit12rds@gmail.com>
2023-07-24 22:53:48 +0530

    folder structure add with settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
-  singleQuote: true,
   trailingComma: 'all',
+  semi: true, // adding semicolons at the end
+  tabWidth: 2,
+  singleQuote: false, // quotes for strings
+  bracketSameLine: false, // add the closing bracket of elements on the next line
+  arrowParens: "always" // arrow function paranthesis for better styling
 };
